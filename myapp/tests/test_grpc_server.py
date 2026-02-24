478657959f5d723fc4556d3366b41c5b3ddb3d5c
mahmoudbarzegar <hedesh.mb@gmail.com>
2026-02-24 17:14:22 +0330

    The test_grpc_server was added.

---
+++ myapp/tests/test_grpc_server.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+class gRPCTestClass(TestCase):
+
+    def test_call_grpc(self):
+        """Test GET request to call_grpc endpoint."""
+        response = self.client.get('/myapp/call-grpc/')
+
+         # Check status code
+        self.assertEqual(response.status_code, 200)
+
