64c035367ce64a575208a5cf946e1b656d630e69
Mahmoud Barzegar <m.bardehgar.hedeshi@gmail.com>
2026-04-26 22:54:00 +0330

    Revert the test_user.py

--- myapp/tests/test_user.py
+++ myapp/tests/test_user.py
@@ -1,7 +1,5 @@
 from django.test import TestCase
 
-from myapp.models import UserModel
-
 class UserTestClass(TestCase):
 
     def test_user_creation(self):
@@ -18,20 +16,4 @@ class UserTestClass(TestCase):
 
          # Check status code
         self.assertEqual(response.status_code, 201)
-
-
-    def test_user_get_password(self):
-        print("Hi")
-        user = UserModel(username="myuser",password="mypassword123",email="mahmodud-twm-2026@local.com")
-        print("Hi 2")
-
-        user.save()
-
-        print("Hi 3")
-
-        user_info = UserModel.objects.get(id=user.id)
-
-        print("Encrypted in DB:", user_info.password)
-        print("Decrypted plain:", user_info.get_password())
-
 
